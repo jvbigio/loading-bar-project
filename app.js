4da09db040e3f1e34b5ecac92be61729bd3e3bb1
Joe Bigio <beej@Joes-MacBook-Pro.local>
2019-08-07 12:54:41 -0400

    Working on different solutions

--- app.js
+++ app.js
@@ -1,16 +1,56 @@
+'use strict';
 // create a loading bar when page loads
 const loadingBarItemEl = document.querySelector('.loading-bar__item');
+// const thirtyButtonEl = document.querySelector('.thirty-percent');
+// const sixtyButtonEl = document.querySelector('.thirty-percent');
+// const hundredButtonEl = document.querySelector('.hundred-percent');
+const buttonEl = document.getElementsByClassName('button');
+const buttonContainerEl = document.querySelector('.button__container');
+
+
+function buttonProgress() {
+  buttonContainerEl.addEventListener('click', function(event) {
+    if (event.target === this.firstElementChild) {
+      loadingBarItemEl.style.width = `${'30%'}`;
+    } else if (event.target === this.children[1]) {
+      loadingBarItemEl.style.width = `${'60%'}`;
+    } else if (event.target === this.lastElementChild) {
+      loadingBarItemEl.style.width = `${'100%'}`;
+    }
+  });
+}
+
+buttonProgress();
+
+
+
+
 // make width of loadingBarEl bigger as time goes on:
 
-let width = 1;
-let loadingBar = setInterval(function() {
-  if (width >= 100) {
-    clearInterval(loadingBar);
-  } else {
-    width++;
-    loadingBarItemEl.style.width = width + '%';
-  }
-}, 10)
+// let width = 0;
+// let loadingBar = setInterval(function() {
+//   if (width >= 100) {
+//     clearInterval(loadingBar);
+//   } else {
+//     width++;
+//     loadingBarItemEl.style.width = width + '%';
+//   }
+// }, 10)
+
+
+// function thirtyPercent() {
+//   thirtyButtonEl.addEventListener('click', function() {
+//     if (event.target.classList.contains(th)) {
+//       clearInterval(loadingBar);
+//     } else {
+//       width++;
+//       loadingBarItemEl.style.width = width;
+//     }
+//   }, 10)
+// }
+
+// thirtyPercent();
+
 
 // let count = 0;
 // let incrementor = setInterval(function() {
@@ -27,3 +67,6 @@ let loadingBar = setInterval(function() {
 // when click on 30%, bar should show only 30%
 //60% up to 60%, etc
 // add eventlistener for each button, then load to each position
+
+// ** TRY: completing challenge using css class .thirty, .sixty and 
+// use classList.add('.thirty);
